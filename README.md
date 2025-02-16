54a30eb822a979a3e5c0bcf7f9bc6a33367d7413
Lowengeist <43280164+Lowengeist@users.noreply.github.com>
2025-02-16 14:20:48 +0100

    Added a few prefixes + indented code

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # shellcode-filter
 Stupid tool to help visualize what instructions are available from a limited set of bytes, badly written in x64.
 
+WIP.
+
+## Usage
+
+To build, you can use
+```
+make
+```
+To run you can either provide the set of bytes that are available to the building of your shellcode, or provide the ones you want to exclude :
+```shell
+./shellcode-filter -i 4143      # only include the bytes 0x41 and 0x43
+./shellcode-filter -x 70        # exclude the byte 0x70
+```
+
+## One last word
+
+Cool ressources:
+- https://wiki.osdev.org/X86-64_Instruction_Encoding
+- https://www.felixcloutier.com/x86/
+
 Any feedback would be greatly appreciated.
--- shellcode-filter.asm
+++ shellcode-filter.asm
@@ -1,33 +1,72 @@
 extern printf
 extern strcmp
+extern fflush
 
 global _start
 
 section .rodata
+; Argument parsing
 exclude_flag:
-    db      "-x", 0
+db  "-x", 0
 include_flag:
-    db      "-i", 0
+db  "-i", 0
+
+; Status
 status_template_str:
-    db      0x09, "[", 0x1b, "[1;%dm%s", 0x1b, "[0;39m] %s", 0x0a, 0
+db  0x09, "[", 0x1b, "[1;%dm%s", 0x1b, "[0;39m] %s", 0x0a, 0
 unavailable_str:
-    db      "X Unavailable", 0     
+db  "X Unavailable", 0     
 available_str: 
-    db      "✓ Available", 0
-prefixes_section_str:
-    db      "-------- PREFIXES --------", 0x0a, 0
-sizes_str:
-    db      "Operand size override:", 0x0a, 0
-operand_size_override_16_32_str:
-    db      "Change operand size from 32 bit to 16 bit, or the contrary (D bit)", 0x0a, 0
+db  " ✓ Available ", 0
+
+section_prefixes_str:
+db  "-------- PREFIXES --------", 0x0a, 0
+    rex_str:
+;    db  "REX:", 0x0a, 0
+;        rexw_str:
+;        db  "Use 64-bit operand size", 0
+;        rexr_str:
+;        db  "Extension to the MODRM.reg field", 0
+;        rexx_str:
+;        db  "Extension to the SIB.index field", 0
+;        rexb_str:
+;        db  "Extension to the MODRM.rm field or the SIB.base field", 0
+    sizes_str:
+    db  "Sizes:", 0x0a, 0
+        operand_size_override_16_32_str:
+        db  "Change operand size from 32 bit to 16 bit, or the contrary (D bit)", 0
+        address_size_override_16_32_str:
+        db  "Change address size from 64 bit to 32 bit, or 32 to 16, or 16 to 32", 0
+    reps_str:
+    db  "Repetitions:", 0x0a, 0
+        rep_str:
+        db  "REP, REPE/REPZ", 0
+        repne_str:
+        db  "REPNE/REPNZ", 0
+    segments_str:
+    db  "Segments:", 0x0a, 0
+        cs_str:
+        db  "CS", 0
+        ss_str:
+        db  "SS", 0
+        ds_str:
+        db  "DS", 0
+        es_str:
+        db  "ES", 0
+        fs_str:
+        db  "FS", 0
+        gs_str:
+        db  "GS", 0
+
+; Errors
 bad_args:
-    db      "Usage: ./shellcode-filter [-ix] bytes", 0x0a, 0
+db  "Usage: ./shellcode-filter [-ix] bytes", 0x0a, 0
 invalid_byte:
-    db      "Error : Given bytes are not valid hex", 0x0a, 0
+db  "Error : Given bytes are not valid hex", 0x0a, 0
 
 section .bss
 available:
-    resb    32
+resb    32
 
 section .text
 print_status:
@@ -137,23 +176,101 @@ process_byte:
     jnz     loop1
 
     ; Prefixes
-    lea     rdi, [prefixes_section_str]
+    lea     rdi, [section_prefixes_str]
     xor     rax, rax
     call    printf
 
-    lea     rdi, [sizes_str]
-    xor     rax, rax
-    call    printf
+        lea     rdi, [sizes_str]
+        xor     rax, rax
+        call    printf
 
-    ; 0x66
-    lea     rdi, [operand_size_override_16_32_str]
-    
-    xor     rsi, rsi
-    mov     rbx, [available + 12]
-    bt      rbx, 0x1
-    setc    sil
+            ; 0x66
+            lea     rdi, [operand_size_override_16_32_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 12]
+            bt      rbx, 1
+            setc    sil
+            call    print_status
 
-    call    print_status
+            ; 0x67
+            lea     rdi, [address_size_override_16_32_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 12]
+            bt      rbx, 0
+            setc    sil
+            call    print_status
+
+        lea     rdi, [reps_str]
+        xor     rax, rax
+        call    printf
+            
+            ; 0xf2
+            lea     rdi, [repne_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 30]
+            bt      rbx, 5
+            setc    sil
+            call    print_status
+
+            ; 0xf3
+            lea     rdi, [rep_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 30]
+            bt      rbx, 4
+            setc    sil
+            call    print_status
+
+        lea     rdi, [segments_str]
+        xor     rax, rax
+        call    printf
+
+            ;0x2e
+            lea     rdi, [cs_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 5]
+            bt      rbx, 1
+            setc    sil
+            call    print_status
+
+            ;0x36
+            lea     rdi, [ss_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 6]
+            bt      rbx, 1
+            setc    sil
+            call    print_status
+
+            ;0x3e
+            lea     rdi, [ds_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 7]
+            bt      rbx, 1
+            setc    sil
+            call    print_status
+
+            ;0x26
+            lea     rdi, [es_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 4]
+            bt      rbx, 1
+            setc    sil
+            call    print_status
+
+            ;0x64
+            lea     rdi, [fs_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 12]
+            bt      rbx, 3
+            setc    sil
+            call    print_status
+
+            ;0x65
+            lea     rdi, [gs_str]
+            xor     rsi, rsi
+            mov     rbx, [available + 12]
+            bt      rbx, 2
+            setc    sil
+            call    print_status
 
     ; Others coming later...
 
@@ -177,5 +294,9 @@ err_invalid_byte:
     call    exit
 
 exit:
+    mov     rbx, rdi
+    xor     rdi, rdi
+    call    fflush
+    mov     rdi, rbx
     mov     rax, 0x3c               ; exit syscall
     syscall
